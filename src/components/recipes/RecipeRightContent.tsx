54037369eb3c67880685c56bf2d62bf169a75ff7
Adam <dapojiabade66@gmail.com>
2025-09-16 17:00:17 +0100

    Enhances responsive SVG clip paths for better rendering
    Refines SVG clip paths for different screen sizes to improve the visual presentation of components.
    
    Adds conditional rendering of clip paths for mobile and medium screens, ensuring a more adaptable layout.
    
    Updates class names for better clarity and responsiveness in the image styling.

--- src/components/recipes/RecipeRightContent.tsx
+++ src/components/recipes/RecipeRightContent.tsx
@@ -11,7 +11,28 @@ const RecipeRightContent = () => {
         <svg width="0" height="0">
           <defs>
             <clipPath id="curveB" clipPathUnits="objectBoundingBox">
-              <path d="M0.0667 0.1 L0.4 0.1 A0.0667 0.05 0 0 0 0.4667 0.05 L0.4667 0.05 A0.0667 0.05 0 0 1 0.5333 0 L0.9333 0 A0.0667 0.05 0 0 1 1 0.05 L1 0.95 A0.0667 0.05 0 0 1 0.9333 1 L0.0667 1 A0.0667 0.05 0 0 1 0 0.95 L0 0.15 A0.0667 0.05 0 0 1 0.0667 0.1 Z"></path>
+              {/* Lg */}
+              <path
+                className="max-lg:hidden"
+                d="M0.0667 0.1 L0.4 0.1 A0.0667 0.05 0 0 0 0.4667 0.05 L0.4667 0.05 A0.0667 0.05 0 0 1 0.5333 0 L0.9333 0 A0.0667 0.05 0 0 1 1 0.05 L1 0.95 A0.0667 0.05 0 0 1 0.9333 1 L0.0667 1 A0.0667 0.05 0 0 1 0 0.95 L0 0.15 A0.0667 0.05 0 0 1 0.0667 0.1 Z"
+              ></path>
+              {/* Md */}
+              <path
+                className="lg:hidden"
+                d="M0.0667 0.1 
+                L0.4 0.1 
+                A0.0667 0.05 0 0 0 0.4667 0.05 
+                L0.4667 0.05 
+                A0.0667 0.05 0 0 1 0.5333 0 
+                L0.9333 0 
+                A0.0667 0.05 0 0 1 1 0.05 
+                L1 0.45 
+                A0.0667 0.05 0 0 1 0.9333 0.5 
+                L0.0667 0.5 
+                A0.0667 0.05 0 0 1 0 0.45 
+                L0 0.15 
+                A0.0667 0.05 0 0 1 0.0667 0.1 Z"
+              />
             </clipPath>
           </defs>
         </svg>
@@ -32,7 +53,8 @@ const RecipeRightContent = () => {
         <svg width="0" height="0">
           <defs>
             <clipPath id="curveB2" clipPathUnits="objectBoundingBox">
-              <path d="M0 0.3 Q0 0.2 0.091 0.2 L0.636 0.2 Q0.727 0.2 0.727 0.1 0.727 0 0.818 0 L0.909 0 Q1 0 1 0.1 L1 0.9 Q1 1 0.909 1 L0.091 1 Q0 1 0 0.9 L0 0.3"></path>
+              {/* <path d="M0 0.3 Q0 0.2 0.091 0.2 L0.636 0.2 Q0.727 0.2 0.727 0.1 0.727 0 0.818 0 L0.909 0 Q1 0 1 0.1 L1 0.9 Q1 1 0.909 1 L0.091 1 Q0 1 0 0.9 L0 0.3"></path> */}
+              <path d="M0 .3 Q0 .2 .091 .2 L.636 .2 Q.727 .2 .727 .1 Q.727 0 .818 0 L.909 0 Q1 0 1 .1 L1 .7 Q1 .8 .909 .8 L.091 .8 Q0 .8 0 .7 Z" />
             </clipPath>
           </defs>
         </svg>
@@ -58,7 +80,7 @@ const RecipeRightContent = () => {
       <img
         src={explore}
         alt="Exploring food"
-        className="aspect-[1/0.8] object-cover max-[650px]:[clip-path:url(#curveB2)] min-[650px]:[clip-path:url(#curveB)] lg:h-full"
+        className="aspect-[1/0.8] object-cover max-[650px]:[clip-path:url(#curveB2)] min-[650px]:[clip-path:url(#curveB)] md:aspect-square lg:h-full"
       />
     </div>
   );
