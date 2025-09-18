6fe64f2fc01bfc6ecd6615c4607cb5c2c4c1c89b
Adam <dapojiabade66@gmail.com>
2025-09-18 21:28:02 +0100

    Moves HowItWorks component to new directory
    Moves the HowItWorks component to a more appropriate directory.
    This change helps to improve the organization and maintainability
    of the project structure.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Demo from "./components/Demo";
 import Footer from "./components/Footer";
 import HealthyInsights from "./components/HealthyInsights";
 import Hero from "./components/hero/Hero";
-import HowItWorks from "./components/howitworks/HowItWorks";
+import HowItWorks from "./components/how-it-works/HowItWorks";
 import Navbar from "./components/navbar/Navbar";
 import NeedAssistance from "./components/NeedAssistance";
 import Pricing from "./components/Pricing";
